2f01ac26eb0a0f45bf76dea212e859cdda268341
ay-sql <e.sqli.ayoub@gmail.com>
2023-11-10 15:10:52 +0100

    dashboard seller edit

--- client/src/containers/DashboardProducts.jsx
+++ client/src/containers/DashboardProducts.jsx
@@ -411,7 +411,7 @@ export default function DashboardProducts() {
         label="Dense padding"
       />
     
-    <button onClick={toggleAddCar} class="bg-zinc-800 float-right large:w-1/6 small:w-1/3 justify-center flex items-center hover:bg-zinc-600 duration-300 text-white small:text-xs text-sm  font-medium small:py small:px-1 py-2 px-4 border border-gray-400 rounded shadow">
+    <button onClick={toggleAddCar} class="bg-zinc-800 float-right large:w-1/6 small:w-1/3 justify-center flex items-center hover:bg-zinc-600 duration-300 text-white small:text-xs large:text-sm  font-medium small:py small:px-1 py-2 px-4 border border-gray-400 rounded shadow">
     <IoAddOutline size={18}  className='mr-2'/>   Add a car 
     </button>
     </Box>
